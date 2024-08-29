c7b8ecd35694b1bfc57be30faf93d377fe309d6d
Aj052001 <ajsingh052001@gmail.com>
2024-08-29 20:55:25 +0530

    update 4

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
-// const puppeteer = require('puppeteer-core');
 const { Client } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
-// Initialize Puppeteer and launch the browser/
-  // Create a new client instance and pass the browser instance
-  const client = new Client();
+const client = new Client();
 
-  client.on('ready', () => {
+client.on('ready', () => {
     console.log('Client is ready!');
-  });
+});
 
-  client.on('qr', qr => {
-    qrcode.generate(qr, { small: true });
-  });
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
 
-  // Listening to all incoming messages
-  client.on('message_create', message => {
-    console.log(message.body);
-  });
+// Listening to all incoming messages
+client.on('message_create', message => {
+	console.log(message.body);
+});
 
-  client.on('message_create', message => {
-    if (message.body === 'hii') {
-      // send back "pong" to the chat the message was sent in
-      client.sendMessage(message.from, 'pong');
-    }
-  });
+client.on('message_create', message => {
+	if (message.body === 'hii') {
+		// send back "pong" to the chat the message was sent in
+		client.sendMessage(message.from, 'pong');
+	}
+});
 
-  // Initialize the WhatsApp client
-  client.initialize();
 
+client.initialize();
